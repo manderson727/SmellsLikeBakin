0cedb1affbdc04b93acc507d970b48c2640118a1
manderson727 <manderson_311@yahoo.com>
2018-01-01 20:42:15 -0500

    Create Fragment

---
+++ app/src/main/java/com/trale/smellslikebakin/ListFragment.java
@@ -0,0 +1,30 @@
+package com.trale.smellslikebakin;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+/**
+ * Created by Mike on 1/1/2018.
+ */
+
+public class ListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //The view returned by this method is what will displayed by our Fragment
+        //return super.onCreate(inflater, container, savedInstanceState);
+        //We need to return a view that represents our fragment_list layout
+        //The LayoutInflater is used to turn XML layouts to Views
+        //second argument - view group - where our new view gets added
+        View view = inflater.inflate(R.layout.fragment_list, container);
+
+        return view;
+    }
+}
