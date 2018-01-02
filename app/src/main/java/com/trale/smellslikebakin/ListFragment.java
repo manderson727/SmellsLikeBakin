a2cbb2efb1c13cbfa02d13e9b626ee2958ac3620
manderson727 <manderson_311@yahoo.com>
2018-01-01 21:36:28 -0500

    Working example

--- app/src/main/java/com/trale/smellslikebakin/ListFragment.java
+++ app/src/main/java/com/trale/smellslikebakin/ListFragment.java
@@ -4,6 +4,8 @@ package com.trale.smellslikebakin;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +28,14 @@ public class ListFragment extends Fragment {
         //third argument - determines whether or not we attached this View to the ViewGroup provided in the second parameter
             //If not set to false, it'll be added twice and we'll get an error.
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        //Hook up recycler view
+        RecyclerView recyclerView = view.findViewById(R.id.listRecyclerView);
+        ListAdapter listAdapter = new ListAdapter();
+        recyclerView.setAdapter(listAdapter);
+        //Fragments always have access to their activity through getActivity or getContext
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
 
         return view;
     }
